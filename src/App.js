54bdc266cc5fbbee7b289dbb825b688f77069f51
Abu Nasim Lomani <info.abunasim@gmail.com>
2021-02-26 20:46:09 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const product = [
+    { name: 'Adobe PhotoShop CC', price: '129.00' },
+    { name: 'Adobe Premium Pro', price: '89.00' },
+    { name: 'Camtasia 9', price: '1239.00' },
+    { name: 'Autacity 1.0', price: '39.00' },
+  ]
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Api></Api>
+        {product.map(pd => <Product product={pd}></Product>)}
       </header>
     </div>
   );
 }
+
+
+function Api() {
+  const [users,setUsers]=useState([]);
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res=>res.json())
+    .then(data=>setUsers(data))
+  });
+  return (
+    <div>
+      <h1>hi:{users.length}</h1>
+      <ul>
+        {
+          users.map(user=><li>{user.email}</li>)
+         
+        }
+        {
+           users.map(user=><li>{user.phone}</li>)
+        }
+      </ul>
+    </div>
+  )
+}
+
+// function Api() {
+//   const [users,setUsers]=useState([]);
+//   useEffect(()=>{
+//     fetch('https://jsonplaceholder.typicode.com/users')
+//     .then(res=>res.json())
+//     .then(data=>setUsers(data))
+//   })
+//   return (
+//     <div>
+//       <h1>Total User:{users.length}</h1>
+//       <ul>
+//         {
+//           users.map(user=><li>{user.name}</li>)
+//         }
+//       </ul>
+//     </div>
+//   )
+// }
+
+function Product(props) {
+  const productStyle = {
+    width: '400px',
+    height: '300px',
+    border: '2px solid gold'
+  }
+  var { name, price } = props.product;
+  return (
+    <div style={productStyle}>
+      <h3>Name:{name}</h3>
+      <h3>Price: ${price}</h3>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit !</p>
+      <button type="submit">Buy now</button>
+    </div>
+  )
+}
+
 
 export default App;
